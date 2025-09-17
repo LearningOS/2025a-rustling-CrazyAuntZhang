e26e583cc5abb8728780b1affd9a92b4de42b3d9
CrazyAuntZhang <781476412@qq.com>
2025-09-18 02:25:45 +0800

    12

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,16 +5,23 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod macros {
+    // 使用#[macro_export]标记宏，允许它被重导出
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+    
+    // 现在可以正确地重导出宏了
+    pub use my_macro;
 }
+
+// 导入模块中的宏
+
 
 fn main() {
     my_macro!();
 }
+    
